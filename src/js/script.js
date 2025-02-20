5fed9d1c69cd2f5b2cb4ab3dc626132a5923b4df
Annika Gadman <annika.gadman@gmail.com>
2025-02-20 08:57:22 -0600

    lagt till animering

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('show');
     });
 });
+
+//Animation
+document.querySelector(".animate-btn").addEventListener("click", function() {
+    const interactiveBox = document.querySelector(".interactive-box");
+    interactiveBox.classList.toggle("active");
+
+    let circle1 = document.querySelector(".extra-circle-1");
+    let circle2 = document.querySelector(".extra-circle-2");
+
+    if (circle1 && circle2) {
+        circle1.remove();
+        circle2.remove();
+    } else {
+        circle1 = document.createElement("div");
+        circle2 = document.createElement("div");
+
+        circle1.classList.add("extra-circle", "extra-circle-1");
+        circle2.classList.add("extra-circle", "extra-circle-2");
+
+        const container = document.querySelector(".animated-btn-wrapper");
+        container.appendChild(circle1);
+        container.appendChild(circle2);
+    }
+});
